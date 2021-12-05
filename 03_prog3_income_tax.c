51c44d1fd9d6317405717a85b85442f32829057a
Ananya Dutta <89250798+AnanyaDutta16@users.noreply.github.com>
2021-12-05 23:55:42 +0530

    Add files via upload

---
+++ 03_prog3_income_tax.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+    int tax=0, income;
+    printf("Enter your income: \n");
+    scanf("%d", &income);
+
+    if(income>=250000 && income<=500000){
+        tax = tax + 0.05*(income-250000);
+    }
+    if(income>=500000 && income<=1000000){
+        tax = tax + 0.2*(income-500000);
+    }
+    if(income>=1000000){
+        tax = tax + 0.3*(income-1000000);
+    }
+    printf("Total tax payable: %d", tax);
+    return 0;
+}
